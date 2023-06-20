9fda049970a542b4cc9924f55b5807e18eba7944
noahGms <noah@keshin.net>
2023-06-20 10:01:53 +0200

    use also token from header in api-auth apps

--- apps/api-auth/src/middleware/auth.js
+++ apps/api-auth/src/middleware/auth.js
@@ -3,9 +3,11 @@ import jwt from 'jsonwebtoken';
 
 export const isAuth = async (req, res, next) => {
   try {
-    const token = req.cookies.token_bearer;
+    const tokenFromHeader = req.headers.authorization ? req.headers.authorization.split(' ')[1] : null;
+    const tokenFromCookie = req.cookies.token_bearer;
+    const token = tokenFromHeader || tokenFromCookie;
 
-    if (!token) return res.status(401).json({error: 'unauthorized'});
+    if (!token) return res.status(401).json({ error: 'unauthorized' });
 
     const decodedToken = await jwt.decode(token, process.env.JWT_SECRET);
     const user = await User.findById(decodedToken.id);
