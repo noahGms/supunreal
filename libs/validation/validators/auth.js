7e6b63054aa624daab2a1f3e31bac4d8dadff8a7
noahGms <noah@keshin.net>
2023-06-20 10:03:25 +0200

    add password_confirmation in registerSchema in auth validator libs

--- libs/validation/validators/auth.js
+++ libs/validation/validators/auth.js
@@ -4,6 +4,7 @@ export const registerSchema = Joi.object({
   username: Joi.string().required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
+  password_confirmation: Joi.string().valid(Joi.ref('password')).required(),
 });
 
 export const loginSchema = Joi.object({
